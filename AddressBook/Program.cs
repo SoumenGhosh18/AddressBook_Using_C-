91cdbcee030a978b52235c125f145ff6027c8ea5
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-17 19:51:22 +0530

    Added: Details of contact person

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,7 +6,23 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Welcome To AddressBook Management");
+            //String fname, lname, address, city, state, zip, phone;
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(firstName);
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number");
+            string phoneNumber = Console.ReadLine();
         }
     }
 }
